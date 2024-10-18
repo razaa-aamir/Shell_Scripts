6593d81734c30af1f44ee9333fc787dff0bef31c
Aamir Raza <razaaamir190@gmail.com>
2024-10-19 00:32:36 +0530

    Create install_package.sh

---
+++ install_package.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+<<note
+
+This script will install any package passed as argument
+./install_package.sh <arg>
+note
+
+echo"************************INSTALLING $1 *********************"
+
+sudo apt-get install
+sudo apt-get install $1 -y
+
+sudo systemctl start $1
+sudo systemctl enable $1
+
+echo "*******************Package Installed*******************"
